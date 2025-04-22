fed0323c608cd29e1ed59b5ed79e690864af9f79
Dereck-Baksa <dereck.baksa@sptech.school>
2025-04-22 11:15:52 -0300

    Codigo arduino

--- Arduino/codigoArduino.ino
+++ Arduino/codigoArduino.ino
@@ -1,9 +1,7 @@
   #include "DHT.h"                      // importando a biblioteca
 #define TIPO_SENSOR DHT11
 const int PINO_SENSOR_DHT11 = A0;     // defnindo a porta de captura - variavel do tipo inteiro
-      
-const int max_u = 100;                // limite max umidade
-const int min_u = 20;                 // limite min umidade
+
 
 DHT sensorDHT(PINO_SENSOR_DHT11, TIPO_SENSOR);  // chama a função da biblioteca
 
@@ -21,7 +19,6 @@ void loop() {
     Serial.println("ERRO ao ler os dados do sensor"); // mensage de erro
   }else {        // label umidade
     Serial.println(umidade);          // exibe o dado de umidade              // separa para a proxima label
-      // exibe o dado fixo min de umidade para determinar limite
     
 delay(1000); // espera 1 segundo para finalizar
   }
