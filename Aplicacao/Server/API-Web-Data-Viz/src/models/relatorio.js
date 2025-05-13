55e369b68d309c19a27fa16fca8a7068adf77460
thaiskheyla <kheyla.paucara@sptech.school>
2025-05-13 14:19:52 -0300

    Grafico dinamico media

--- Aplicacao/Server/API-Web-Data-Viz/src/models/relatorio.js
+++ Aplicacao/Server/API-Web-Data-Viz/src/models/relatorio.js
@@ -8,10 +8,26 @@ var caminho_env = ambiente_processo === 'producao' ? '.env' : '.env.dev';
  // require("dotenv").config({ path: caminho_env });
 
 var database = require("../database/config");
+var vetorMes = []
+
+import {select_unidade} from '../../public/Dashboard/relatorio.html';
 
 function buscarUltimasMedidas() {
 
-    var instrucaoSql = `select umidade from medicao;`;
+    var dataAtual = new Date();
+    var mesAtual = dataAtual.getMonth();
+    var anoAtual = dataAtual.getFullYear();
+    var unidadeAtual = select_unidade();
+
+
+    var instrucaoSql = `select avg (umidade)
+from sensor
+inner join medicao on id_sensor = sensor.id
+where id_unidade = ${unidadeAtual} and
+AND DATE(data_hora) BETWEEN '${anoAtual}-${mesAtual}-01' 
+AND 'LAST_DAY(${anoAtual}-${mesAtual}-01')`;
+
+vetorMes[mesAtual] = instrucaoSql
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
