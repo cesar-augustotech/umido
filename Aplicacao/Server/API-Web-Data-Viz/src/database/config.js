bd4a592bcd07435c368d2d150c87e00cd2715c22
BillHebertPDC <bill.choi@sptech.school.com>
2025-05-15 00:10:00 -0300

    14-05

--- Aplicacao/Server/API-Web-Data-Viz/src/database/config.js
+++ Aplicacao/Server/API-Web-Data-Viz/src/database/config.js
@@ -2,7 +2,6 @@ var mysql = require("mysql2");
 
 // CONEXÃO DO BANCO MYSQL SERVER
 /*Bill 
-*/
 var mySqlConfig = {
     host: 'localhost',
     database: 'umido',
@@ -11,6 +10,7 @@ var mySqlConfig = {
     port: "3306"
 };
 /*
+*/
 var mySqlConfig = {
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
@@ -18,7 +18,7 @@ var mySqlConfig = {
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT
 };
-*/
+
 
 function executar(instrucao) {
 
