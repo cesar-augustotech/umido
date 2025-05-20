614fcf7b31b0dfa261abd6b09ad46c90bbb5fc5a
Dereck-Baksa <dereck.baksa@sptech.school>
2025-05-20 10:24:17 -0300

    routes relatorio

--- Aplicacao/Server/API-Web-Data-Viz/src/routes/relatorio.js
+++ Aplicacao/Server/API-Web-Data-Viz/src/routes/relatorio.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const router = express.Router();
 
+const controller = require('../controllers/relatorioController');
+
 const relatorioController = require('../controllers/relatorioController');
 
 
 router.get("/buscarUmidadeMediaUnidade/:unidadeAtual", function (req, res) {
     relatorioController.buscarUmidadeMediaUnidade(req, res);
 });
+
+
+router.get("/buscarAlertas/:unidadeAtual", function (req, res) {
+    relatorioController.buscarAlertas(req, res);
+});
+
+
+
+router.get("/buscarIncidentes/:unidadeAtual", function (req, res) {
+    relatorioController.buscarIncidentes(req, res);
+});
+
+
+
+router.get("/buscarUnidadeMediaPorSemana/:unidadeAtual", function (req, res) {
+    relatorioController.buscarUnidadeMediaPorSemana(req, res);
+});
+
+
+
+
 
 
 module.exports = router;
